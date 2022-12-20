4af2158713107220e2e3664b114800a2eff13f45
NANANG ADI-UTOMO <utomo17121999@gmail.com>
2022-12-20 10:57:44 +0700

    jk

--- src/components/registercomp.vue
+++ src/components/registercomp.vue
@@ -16,30 +16,42 @@
                                 <p class="text-center fw-bold mx-3 mb-0">Daftar !</p>
                             </div>
 
-                            <div class="d-flex">
+                            <div >
                                 <div class="comp">
                                     <div>
-                                        <div class="form-row mb-2">
+                                        <div class="inputGroup">
+                                            <input v-model="regisdata.nama" type="text" required="" autocomplete="off">
+                                            <label for="name">Nama Lengkap</label>
+                                        </div>
+                                        <!-- <div class="form-row mb-2">
                                             <label class="form-label" for="form3Example3">Nama Lengkap</label>
                                             <div class="col-sm-12 ">
                                                 <input v-model="regisdata.nama"
                                                 type="text"  class="form-control form-control-lg mr-3"
                                                 placeholder="Masukkan Nama Anda" />
                                             </div>
-                                        </div>
+                                        </div> -->
                                     </div>
                                     <div>
-                                        <div class="form-row mb-2">
+                                        <div class="inputGroup">
+                                            <input v-model="regisdata.email" type="text" required="" autocomplete="off">
+                                            <label for="name">Alamat Email</label>
+                                        </div>
+                                        <!-- <div class="form-row mb-2">
                                             <label class="form-label" for="form3Example3">E-mail</label>
                                             <div class="col-sm-12 ">
                                                 <input v-model="regisdata.email" 
                                                  type="email"  class="form-control form-control-lg"
                                                 placeholder="Masukkan Email anda" />
                                             </div>
-                                        </div>
+                                        </div> -->
                                     </div>
                                     <div>
-                                        <div class="form-row mb-2">
+                                        <div class="inputGroup">
+                                            <input v-model="regisdata.password" type="password" required="" autocomplete="off">
+                                            <label for="name">Password</label>
+                                        </div>
+                                        <!-- <div class="form-row mb-2">
                                             <label class="form-label" for="form3Example3">Password</label>
                                             <div class="col-sm-12 ">
                                                 <input 
@@ -47,10 +59,14 @@
                                                 type="password" id="form3Example3" class="form-control form-control-lg"
                                                 placeholder=" Password Anda" />
                                             </div>
-                                        </div>
+                                        </div> -->
                                     </div>
                                     <div>
-                                        <div class="form-row mb-2">
+                                        <div class="inputGroup">
+                                            <input v-model="password2" type="password" required="" autocomplete="off">
+                                            <label for="name">Re-Password</label>
+                                        </div>
+                                        <!-- <div class="form-row mb-2">
                                             <label class="form-label" for="form3Example3">Re-Password</label>
                                             <div class="col-sm-12 ">
                                                 <input
@@ -58,25 +74,24 @@
                                                 type="password"  class="form-control form-control-lg"
                                                 placeholder="Ulangi Password Anda" />
                                             </div>
-                                        </div>
+                                        </div> -->
                                     </div>
                                 </div>
 
                                 <div>
                                 </div>
                             </div>
-                                <p v-if="Success" class="fw-bold mt-2">Berhasil Daftar !, Silahkan Login !</p>
-                                <p v-if="emailValid" class="textemail">Email Sudah Digunakan !</p>
-                                <p v-if="passwordValid" class="textpass">Password Tidak Sama</p>
-
+                                <!-- <p v-if="Success" class="fw-bold mt-2">Berhasil Daftar !, Silahkan Login !</p>
+                                 <p v-if="emailValid" class="textemail">Email Sudah Digunakan !</p> 
+                                <p v-if="passwordValid" class="textpass">Password Tidak Sama</p> -->
                             <div class="d-flex">
-                                <div class="text-center text-lg-start pt-2 ml-3 mb-4">
+                                <div class="text-center text-lg-start pt-2 ml-3 mb-4 mt-4 mr-5" >
                                     <router-link to="/" type="button" class="btn btn-danger btn-lg text-center"
-                                    style="padding-left: 3 .5rem; padding-right: 4rem;" v-if="btnlogin">login</router-link>
+                                     v-if="btnlogin">login</router-link>
                                 </div>
-                                <div class="text-center text-lg-start mt-4 pt-2 mb-5">
+                                <div class="text-center text-lg-start mt-4 pt-2 mb-5 mr-4">
                                     <button type="submit" class="btn btn-primary btn-lg text-center"
-                                    style="padding-left: 3 .5rem; padding-right: 2.6rem;" v-if="btnregister">Register</button>
+                                    v-if="btnregister">Register</button>
                                 </div>
                             </div>
 
@@ -113,13 +128,11 @@ data(){
             "password" : null
         },
         password2 : "",
-        Success : false,
-        emailValid : false,
-        passwordValid : false,
-        btnlogin : false,
-        btnregister : true
-
-        
+        // Success : false,
+        // emailValid : false,
+        // passwordValid : false,
+        btnlogin : true,
+        btnregister : true,
     }
 },
 methods : {
@@ -128,28 +141,32 @@ methods : {
         let password1 = data.password;
         let password2 = this.password2;
 
-        this.Success = false;
-        this.emailValid = false;
-        this.passwordValid = false;
+        // this.Success = false;
+        //  this.emailValid = false;
+        // this.passwordValid = false;
         if(password1 == password2){
             userservice.create(data)
             .then(response =>{ 
                 console.log(response);
                 this.regisdata = {};
                 this.password2 = "";
-                this.Success = true;
+                alert("Register Berhasil Silahkan Login!")
+                // this.Success = true;
                 this.btnlogin = true;
                 this.btnregister = false
             })
             .catch(e => {
                 console.log(e);
                 if(e.response.data.status === 500){
-
-                    this.emailValid = true;
+                    // this.emailValid = true;
+                    alert("Alamat email Sudah digunakan")
+                    location.reload();
                 }
             })
         }else{
-            this.passwordValid = true;
+            // this.passwordValid = true;
+            alert("Password Tidak Sama !")
+                    location.reload();
         }
     }
 },
@@ -161,7 +178,7 @@ methods : {
     border: 2px solid black;
     border-radius: 8px;
     width: 70%;
-    height: 628px;
+    height: 555px;
     margin-left: 15%;
     margin-top: 30px;
     
@@ -179,7 +196,6 @@ methods : {
 .input{
     background-color: bisque;
 }
-
 .divider:after,
 .divider:before {
 content: "";
@@ -196,14 +212,54 @@ height: 100%;
 }
 }
 .comp{
-    justify-content: space-evenly
-    ;
-}
-.textemail{
-    color: red;
-}
-.textpass{
-    color: red;
+    justify-content: space-evenly;
 }
 
+.inputGroup {
+  font-family: 'Segoe UI', sans-serif;
+  margin: 1em 0 1em 0;
+  max-width: 300px;
+  position: relative;
+}
+
+.inputGroup input {
+  font-size: 100%;
+  padding: 0.8em;
+  outline: none;
+  border: 2px solid rgb(200, 200, 200);
+  background-color: transparent;
+  border-radius: 20px;
+  width: 100%;
+}
+
+.inputGroup label {
+  font-size: 100%;
+  position: absolute;
+  left: 0;
+  padding: 0.8em;
+  margin-left: 0.5em;
+  pointer-events: none;
+  transition: all 0.3s ease;
+  color: rgb(100, 100, 100);
+}
+
+.inputGroup :is(input:focus, input:valid)~label {
+  transform: translateY(-50%) scale(.9);
+  margin: 0em;
+  margin-left: 1.3em;
+  padding: 0.4em;
+  background-color:transparent;
+}
+
+.inputGroup :is(input:focus, input:valid) {
+  border-color: rgb(150, 150, 200);
+}
+.btn-danger{
+    margin-right: 27px;
+    width: 135px;
+}
+.btn-primary{
+    width: 135px;
+    
+}
 </style>
