e02d961cd041cbcaeda00ac093680cc47f69a96c
NANANG ADI-UTOMO <utomo17121999@gmail.com>
2022-12-20 15:30:09 +0700

    rty

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -4,17 +4,18 @@
         <div>
             <div class="text">
                 <div class="load">
-                    <div class="loader">
-                        <div class="loader-square"></div>
-                        <div class="loader-square"></div>
-                        <div class="loader-square"></div>
-                        <div class="loader-square"></div>
-                        <div class="loader-square"></div>
-                        <div class="loader-square"></div>
-                        <div class="loader-square"></div>
-                    </div>
+                    <div class="spinner">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        </div>
                 </div>
-                <h1>E-KTP APP</h1>
+                <div class="text">
+                    <h1>E-KTP</h1>
+                </div>
             </div>
             <div class="link">
                 <div class="home">
@@ -52,7 +53,7 @@ export default {
     font-family:'Arial Narrow Bold';
     text-decoration-line :underline;
     text-align: center;
-    margin-top: 30px;
+    margin-top: 20px;
     
 }
 a{
@@ -95,155 +96,114 @@ a{
     height: 50px;
     border-radius: 9px;
     font-family: 'Arial Narrow Bold';
-    margin-top: 115%;
+    margin-top: 130%;
 }
 .out:hover{
     box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, black 0 -3px 0 inset;
     transform: translateY(-2px);
 }
 .load{
-    margin-left: 66px;
-    margin-top: 20px;
+    margin-top: 30px;
+    margin-left: 43%;
+    margin-bottom: 10px;
 }
-@keyframes square-animation {
+.spinner {
+ width: 44px;
+ height: 44px;
+ animation: spinner-y0fdc1 2s infinite ease;
+ transform-style: preserve-3d;
+}
+
+.spinner > div {
+ background-color: rgba(0,77,255,0.2);
+ height: 100%;
+ position: absolute;
+ width: 100%;
+ border: 2px solid #004dff;
+}
+
+.spinner div:nth-of-type(1) {
+ transform: translateZ(-22px) rotateY(180deg);
+}
+
+.spinner div:nth-of-type(2) {
+ transform: rotateY(-270deg) translateX(50%);
+ transform-origin: top right;
+}
+
+.spinner div:nth-of-type(3) {
+ transform: rotateY(270deg) translateX(-50%);
+ transform-origin: center left;
+}
+
+.spinner div:nth-of-type(4) {
+ transform: rotateX(90deg) translateY(-50%);
+ transform-origin: top center;
+}
+
+.spinner div:nth-of-type(5) {
+ transform: rotateX(-90deg) translateY(50%);
+ transform-origin: bottom center;
+}
+
+.spinner div:nth-of-type(6) {
+ transform: translateZ(22px);
+}
+
+.spinner {
+ width: 44px;
+ height: 44px;
+ animation: spinner-y0fdc1 2s infinite ease;
+ transform-style: preserve-3d;
+}
+
+.spinner > div {
+ background-color: rgba(0,77,255,0.2);
+ height: 100%;
+ position: absolute;
+ width: 100%;
+ border: 2px solid #004dff;
+}
+
+.spinner div:nth-of-type(1) {
+ transform: translateZ(-22px) rotateY(180deg);
+}
+
+.spinner div:nth-of-type(2) {
+ transform: rotateY(-270deg) translateX(50%);
+ transform-origin: top right;
+}
+
+.spinner div:nth-of-type(3) {
+ transform: rotateY(270deg) translateX(-50%);
+ transform-origin: center left;
+}
+
+.spinner div:nth-of-type(4) {
+ transform: rotateX(90deg) translateY(-50%);
+ transform-origin: top center;
+}
+
+.spinner div:nth-of-type(5) {
+ transform: rotateX(-90deg) translateY(50%);
+ transform-origin: bottom center;
+}
+
+.spinner div:nth-of-type(6) {
+ transform: translateZ(22px);
+}
+
+@keyframes spinner-y0fdc1 {
  0% {
-  left: 0;
-  top: 0;
- }
-
- 10.5% {
-  left: 0;
-  top: 0;
- }
-
- 12.5% {
-  left: 32px;
-  top: 0;
- }
-
- 23% {
-  left: 32px;
-  top: 0;
- }
-
- 25% {
-  left: 64px;
-  top: 0;
- }
-
- 35.5% {
-  left: 64px;
-  top: 0;
- }
-
- 37.5% {
-  left: 64px;
-  top: 32px;
- }
-
- 48% {
-  left: 64px;
-  top: 32px;
+  transform: rotate(45deg) rotateX(-25deg) rotateY(25deg);
  }
 
  50% {
-  left: 32px;
-  top: 32px;
- }
-
- 60.5% {
-  left: 32px;
-  top: 32px;
- }
-
- 62.5% {
-  left: 32px;
-  top: 64px;
- }
-
- 73% {
-  left: 32px;
-  top: 64px;
- }
-
- 75% {
-  left: 0;
-  top: 64px;
- }
-
- 85.5% {
-  left: 0;
-  top: 64px;
- }
-
- 87.5% {
-  left: 0;
-  top: 32px;
- }
-
- 98% {
-  left: 0;
-  top: 32px;
+  transform: rotate(45deg) rotateX(-385deg) rotateY(25deg);
  }
 
  100% {
-  left: 0;
-  top: 0;
+  transform: rotate(45deg) rotateX(-385deg) rotateY(385deg);
  }
 }
-
-.loader {
- position: relative;
- width: 96px;
- height: 96px;
- transform: rotate(45deg);
-}
-
-.loader-square {
- position: absolute;
- top: 0;
- left: 0;
- width: 28px;
- height: 28px;
- margin: 2px;
- border-radius: 0px;
- background: white;
- background-size: cover;
- background-position: center;
- background-attachment: fixed;
- animation: square-animation 10s ease-in-out infinite both;
-}
-
-.loader-square:nth-of-type(0) {
- animation-delay: 0s;
-}
-
-.loader-square:nth-of-type(1) {
- animation-delay: -1.4285714286s;
-}
-
-.loader-square:nth-of-type(2) {
- animation-delay: -2.8571428571s;
-}
-
-.loader-square:nth-of-type(3) {
- animation-delay: -4.2857142857s;
-}
-
-.loader-square:nth-of-type(4) {
- animation-delay: -5.7142857143s;
-}
-
-.loader-square:nth-of-type(5) {
- animation-delay: -7.1428571429s;
-}
-
-.loader-square:nth-of-type(6) {
- animation-delay: -8.5714285714s;
-}
-
-.loader-square:nth-of-type(7) {
- animation-delay: -10s;
-}
-
 </style>
