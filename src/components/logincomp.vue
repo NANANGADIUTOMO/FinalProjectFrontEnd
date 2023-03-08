b2df1d4540b7a75ce78814e94767be6a67e52a4a
NANANG ADI-UTOMO <utomo17121999@gmail.com>
2023-03-08 11:26:04 +0700

    revisi 7/3/2023

--- src/components/logincomp.vue
+++ src/components/logincomp.vue
@@ -1,132 +1,168 @@
 <template>
-    <div class="login">
-        <form>
-            <div>
-                <section class="vh-98">
-                    <div class="container-fluid h-custom">
-                        <div class="row d-flex justify-content-center align-items-center h-98">    
-                        <div class="col-md-9 col-lg-6 col-xl-5 mt-4">
-                            <img src="../assets/ktp.png"
-                            class="img-fluid" alt="Sample image">
-                        </div>
-                        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                            <form @submit.prevent="login">
-                            <div class="divider d-flex align-items-center my-4">
-                                <p class="text-center fw-bold mx-3 mb-0">Selamat Datang !</p>
-                            </div>
-                            <!-- Email input -->
-                            <div class="inputGroup">
-                                <input v-model="datalogin.email" type="email" required="" autocomplete="off">
-                                <label for="name">Alamat Email</label>
-                            </div>
+  <div class="login">
+    <form>
+      <div>
+        <section class="vh-98">
+          <div class="container-fluid h-custom">
+            <div
+              class="row d-flex justify-content-center align-items-center h-98"
+            >
+              <div class="col-md-9 col-lg-6 col-xl-5 mt-4">
+                <img
+                  src="../assets/ktp.png"
+                  class="img-fluid"
+                  alt="Sample image"
+                />
+              </div>
+              <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <form @submit.prevent="login">
+                  <div class="divider d-flex align-items-center my-4">
+                    <p class="text-center fw-bold mx-3 mb-0">
+                      Selamat Datang !
+                    </p>
+                  </div>
+                  <!-- Email input -->
+                  <div class="inputGroup">
+                    <input
+                      v-model="datalogin.email"
+                      type="email"
+                      required=""
+                      autocomplete="off"
+                    />
+                    <label for="name">Alamat Email</label>
+                  </div>
 
-                            <!-- Password input -->
-                            <div class="inputGroup">
-                                <input v-model="datalogin.password" type="password" required="" autocomplete="off">
-                                <label for="name">Password</label>
-                            </div>
-                            <P v-if="massage" class="texts">Username atau Password Anda Salah</P>
-                                <div class="text-center text-lg-start mt-4 pt-2">
-                                    <button type="submit" class="btn btn-primary btn-lg text-center">login</button>
-                                    <p class="small fw-bold mt-2 pt-1 mb-0">Belum Memiliki Akun? 
-                                        <router-link to="/register" class="link-danger">Buat Akun</router-link></p><p></p>
-                                </div>
-                            </form>
-                        </div>
-                        </div>
-                       
-                    </div>
-                    <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-                        <!-- Copyright -->
-                        <div class="text-white mb-3 mb-md-0">
-                        Copyright © 2022. Build By Nanang.
-                        </div>
-                        
-                    </div>
-                </section>
+                  <!-- Password input -->
+                  <div class="inputGroup">
+                    <input
+                      v-model="datalogin.password"
+                      type="password"
+                      required=""
+                      autocomplete="off"
+                    />
+                    <label for="name">Password</label>
+                  </div>
+                  <P v-if="massage" class="texts"
+                    >Username atau Password Anda Salah</P
+                  >
+                  <div class="text-center text-lg-start mt-4 pt-2">
+                    <button
+                      type="submit"
+                      class="btn btn-primary btn-lg text-center"
+                    >
+                      login
+                    </button>
+                    <p class="small fw-bold mt-2 pt-1 mb-0">
+                      Belum Memiliki Akun?
+                      <router-link to="/register" class="link-danger"
+                        >Buat Akun</router-link
+                      >
+                    </p>
+                    <p></p>
+                  </div>
+                </form>
+              </div>
             </div>
-        </form>
-    </div>
+          </div>
+          <div
+            class="
+              d-flex
+              flex-column flex-md-row
+              text-center text-md-start
+              justify-content-between
+              py-4
+              px-4 px-xl-5
+              bg-primary
+            "
+          >
+            <!-- Copyright -->
+            <div class="text-white mb-3 mb-md-0">
+              Copyright © 2022. Build By Nanang.
+            </div>
+          </div>
+        </section>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-import userservice from '@/service/userservice';
+import userservice from "@/service/userservice";
 export default {
-name:'loginComponent',
-data (){
-    return{
-        datalogin:{
-            "email" : null,
-            "password" : null,
-        },
-        massage : false,
-        load : false
-    }
-},
-methods :{
-    login (){
-        let data = this.datalogin;
-        userservice.login(data)
-        .then(response =>{
-            console.log(response);
-            alert("Login Sukses")
-            this.$router.push("/home")
+  name: "loginComponent",
+  data() {
+    return {
+      datalogin: {
+        email: null,
+        password: null,
+      },
+      massage: false,
+      load: false,
+    };
+  },
+  methods: {
+    login() {
+      let data = this.datalogin;
+      userservice
+        .login(data)
+        .then((response) => {
+          console.log(response);
+          alert("Login Sukses");
+          this.$router.push("/home");
         })
-        .catch(e =>{
-            console.log(e)
-            alert("Nama atau Password Salah!")
-                 location.reload();
-        })
-    }
-}
-  }
-
+        .catch((e) => {
+          console.log(e);
+          alert("Nama atau Password Salah!");
+          location.reload();
+        });
+    },
+  },
+};
 </script>
 
-<style scoped>  
-.login{
-    border: 2px solid black;
-    border-radius: 8px;
-    width: 900px;
-    height: 461px;
-    margin-left: 20%;
-    margin-top: 7%;
-    
+<style scoped>
+.login {
+  border: 2px solid black;
+  border-radius: 8px;
+  width: 900px;
+  height: 457px;
+  margin-left: 20%;
+  margin-top: 7%;
 }
-.texts{
-    color: red;
+.texts {
+  color: red;
 }
-.text{
-    margin-left: 39% ;
+.text {
+  margin-left: 39%;
 }
-.gambar{
-    margin-left: 20px;
-    margin-top: 45px;
+.gambar {
+  margin-left: 20px;
+  margin-top: 45px;
 }
-.konten{
-    display: flex;
+.konten {
+  display: flex;
 }
-.input{
-    background-color: bisque;
+.input {
+  background-color: bisque;
 }
 
 .divider:after,
 .divider:before {
-content: "";
-flex: 1;
-height: 1px;
-background: #eee;
+  content: "";
+  flex: 1;
+  height: 1px;
+  background: #eee;
 }
 .h-custom {
-height: calc(100% - 73px);
+  height: calc(100% - 73px);
 }
 @media (max-width: 450px) {
-.h-custom {
-height: 100%;
-}
+  .h-custom {
+    height: 100%;
+  }
 }
 .inputGroup {
-  font-family: 'Segoe UI', sans-serif;
+  font-family: "Segoe UI", sans-serif;
   margin: 1em 0 1em 0;
   max-width: 300px;
   position: relative;
@@ -153,24 +189,24 @@ height: 100%;
   color: black;
 }
 
-.inputGroup :is(input:focus, input:valid)~label {
-  transform: translateY(-50%) scale(.9);
+.inputGroup :is(input:focus, input:valid) ~ label {
+  transform: translateY(-50%) scale(0.9);
   margin: 0em;
   margin-left: 1.3em;
   padding: 0.4em;
-  background-color:transparent;
+  background-color: transparent;
   /* border-radius: 30px; */
 }
 
 .inputGroup :is(input:focus, input:valid) {
   border-color: black;
 }
-.btn-primary{
-    width: 274px;
+.btn-primary {
+  width: 274px;
 }
-.flex-column{
-    margin-top: 62px;
-    border-bottom-right-radius: 6px 2px;
-    border-bottom-left-radius: 3px 4px;
+.flex-column {
+  margin-top: 62px;
+  border-bottom-right-radius: 6px 2px;
+  border-bottom-left-radius: 3px 4px;
 }
 </style>
--- src/components/registercomp.vue
+++ src/components/registercomp.vue
@@ -1,175 +1,234 @@
 <template>
-     <div class="register">
+  <div class="register">
+    <div>
+      <form>
         <div>
-        <form>
-            <div>
-                <section class="vh-98">
-                    <div class="container-fluid h-custom">
-                        <div class="row d-flex justify-content-center align-items-center h-98">
-                        <div class="col-md-5 col-lg-3 col-xl-5">
-                            <img src="../assets/ktp.png"
-                            class="" alt="Sample image" width="350px">
-                        </div>
-                        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                            <form @submit.prevent="inputregister">
-                            <div class="divider d-flex align-items-center my-4">
-                                <p class="text-center fw-bold mx-3 mb-0">Daftar !</p>
-                            </div>
-
-                            <div >
-                                <div class="comp">
-                                    <div>
-                                        <div class="inputGroup">
-                                            <input v-model="regisdata.nama" type="text" required="Nama Tidak Boleh kosong!" autocomplete="off">
-                                            <label for="name">Nama Lengkap</label>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="inputGroup">
-                                            <input v-model="regisdata.email" type="text" required="" autocomplete="off">
-                                            <label for="name">Alamat Email</label>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="inputGroup">
-                                            <input v-model="regisdata.password" type="password" required="" autocomplete="off">
-                                            <label for="name">Password</label>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="inputGroup">
-                                            <input v-model="password2" type="password" required="" autocomplete="off">
-                                            <label for="name">Re-Password</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div>
-                                </div>
-                            </div>
-                            <div class="d-flex">
-                                <div class="text-center text-lg-start pt-2 ml-3 mb-4 mt-4 mr-5" >
-                                    <router-link to="/" type="button" class="btn btn-danger btn-lg text-center"
-                                     v-if="btnlogin">login</router-link>
-                                </div>
-                                
-                                <div class="text-center text-lg-start mt-4 pt-2 mb-5 mr-4">
-                                    <button type="submit" class="btn btn-primary btn-lg text-center"
-                                    v-if="btnregister">Register</button>
-                                </div>
-                            </div>
-
-                            </form>
-                        </div>
-                        </div>
+          <section class="vh-98">
+            <div class="container-fluid h-custom">
+              <div
+                class="row d-flex justify-content-center align-items-center h-98"
+              >
+                <div class="col-md-5 col-lg-3 col-xl-5">
+                  <img
+                    src="../assets/ktp.png"
+                    class=""
+                    alt="Sample image"
+                    width="350px"
+                  />
+                </div>
+                <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                  <form @submit.prevent="inputregister">
+                    <div class="divider d-flex align-items-center my-4">
+                      <p class="text-center fw-bold mx-3 mb-0">Daftar !</p>
                     </div>
-                    <div
-                        class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-                        <!-- Copyright -->
-                        <div class="text-white mb-3 mb-md-0">
-                        Copyright © 2022. Build By Nanang.
+
+                    <div>
+                      <div class="comp">
+                        <!-- input nama -->
+                        <div>
+                          <div class="inputGroup">
+                            <input
+                              v-model="regisdata.nama"
+                              type="text"
+                              required="Nama Tidak Boleh kosong!"
+                              autocomplete="off"
+                            />
+                            <label for="name">Nama Lengkap</label>
+                          </div>
                         </div>
+
+                        <!-- input email -->
+                        <div>
+                          <div class="inputGroup">
+                            <input
+                              v-model="regisdata.email"
+                              type="text"
+                              required=""
+                              autocomplete="off"
+                            />
+                            <label for="name">Alamat Email</label>
+                          </div>
+                        </div>
+
+                        <!-- input gender -->
+                        <div>
+                          <div class="inputGroup">
+                            <input
+                              v-model="regisdata.gender"
+                              type="text"
+                              required=""
+                              autocomplete="off"
+                            />
+                            <label for="name">Jenis Kelamin</label>
+                          </div>
+                        </div>
+
+                        <!-- input password -->
+                        <div>
+                          <div class="inputGroup">
+                            <input
+                              v-model="regisdata.password"
+                              type="password"
+                              required=""
+                              autocomplete="off"
+                            />
+                            <label for="name">Password</label>
+                          </div>
+                        </div>
+
+                        <!-- input repassword -->
+                        <div>
+                          <div class="inputGroup">
+                            <input
+                              v-model="password2"
+                              type="password"
+                              required=""
+                              autocomplete="off"
+                            />
+                            <label for="name">Re-Password</label>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div></div>
                     </div>
-                </section>
+                    <div class="d-flex">
+                      <div
+                        class="text-center text-lg-start pt-2 ml-3 mb-4 mt-2 mr-5"
+                      >
+                        <router-link
+                          to="/"
+                          type="button"
+                          class="btn btn-danger btn-lg text-center"
+                          v-if="btnlogin"
+                          >login</router-link
+                        >
+                      </div>
+
+                      <div
+                        class="text-center text-lg-start mt-2 pt-2 mb-5 mr-4"
+                      >
+                        <button
+                          type="submit"
+                          class="btn btn-primary btn-lg text-center"
+                          v-if="btnregister"
+                        >
+                          Register
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
             </div>
-        </form>
+            <div
+              class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary"
+            >
+              <!-- Copyright -->
+              <div class="text-white mb-3 mb-md-0">
+                Copyright © 2022. Build By Nanang.
+              </div>
+            </div>
+          </section>
         </div>
+      </form>
     </div>
+  </div>
 </template>
 
 <script>
-
-import userservice from '../service/userservice'
+import userservice from "../service/userservice";
 export default {
-name : 'registerComponent',
-components:{
-},
-data(){
-    return{
-        regisdata:{
-            "email" : null,
-            "nama" : null,
-            "password" : null
-        },
-        password2 : "",
-        btnlogin : true,
-        btnregister : true,
-    }
-},
-methods : {
-    inputregister(){
-        let data = this.regisdata;
-        let password1 = data.password;
-        let password2 = this.password2;
-        if(password1 == password2){
-            userservice.create(data)
-            .then(response =>{ 
-                console.log(response);
-                this.regisdata = {};
-                this.password2 = "";
-                    alert("Register Berhasil Silahkan Login!")
-            })
-            .catch(e => {
-                console.log(e);
-                if(e.response.data.status === 500){
-                    alert("Alamat email Sudah digunakan")
-                    location.reload();
-                }
-            })
-        }else{
-            alert("Password Tidak Sama !")
-                    location.reload();
-        }
-    }
-},
-}
+  name: "registerComponent",
+  components: {},
+  data() {
+    return {
+      regisdata: {
+        email: null,
+        nama: null,
+        gender: null,
+        password: null,
+      },
+      password2: "",
+      btnlogin: true,
+      btnregister: true,
+    };
+  },
+  methods: {
+    inputregister() {
+      let data = this.regisdata;
+      let password1 = data.password;
+      let password2 = this.password2;
+      if (password1 == password2) {
+        userservice
+          .create(data)
+          .then((response) => {
+            console.log(response);
+            this.regisdata = {};
+            this.password2 = "";
+            alert("Register Berhasil Silahkan Login!");
+          })
+          .catch((e) => {
+            console.log(e);
+            if (e.response.data.status === 500) {
+              alert("Alamat email Sudah digunakan");
+              location.reload();
+            }
+          });
+      } else {
+        alert("Password Tidak Sama !");
+        location.reload();
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-.register{
-    border: 2px solid black;
-    border-radius: 8px;
-    width: 70%;
-    height: 555px;
-    margin-left: 15%;
-    margin-top: 45px;
-    position: absolute;
+.register {
+  border: 2px solid black;
+  border-radius: 8px;
+  width: 70%;
+  height: 599px;
+  margin-left: 15%;
+  margin-top: 45px;
+  position: absolute;
 }
 
-.text{
-    margin-left: 39% ;
+.text {
+  margin-left: 39%;
 }
-.gambar{
-    margin-left: 20px;
-    margin-top: 35px;
+.gambar {
+  margin-left: 20px;
+  margin-top: 35px;
 }
-.konten{
-    display: flex;
+.konten {
+  display: flex;
 }
-.input{
-    background-color: bisque;
+.input {
+  background-color: bisque;
 }
 .divider:after,
 .divider:before {
-content: "";
-flex: 1;
-height: 1px;
-background: #eee;
+  content: "";
+  flex: 1;
+  height: 1px;
+  background: #eee;
 }
 .h-custom {
-height: calc(100% - 73px);
+  height: calc(100% - 73px);
 }
 @media (max-width: 450px) {
-.h-custom {
-height: 100%;
+  .h-custom {
+    height: 100%;
+  }
 }
-}
-.comp{
-    justify-content: space-evenly;
+.comp {
+  justify-content: space-evenly;
 }
 
 .inputGroup {
-  font-family: 'Segoe UI', sans-serif;
+  font-family: "Segoe UI", sans-serif;
   margin: 1em 0 1em 0;
   max-width: 300px;
   position: relative;
@@ -196,23 +255,22 @@ height: 100%;
   color: black;
 }
 
-.inputGroup :is(input:focus, input:valid)~label {
-  transform: translateY(-50%) scale(.9);
+.inputGroup :is(input:focus, input:valid) ~ label {
+  transform: translateY(-50%) scale(0.9);
   margin: 0em;
   margin-left: 1.3em;
   padding: 0.4em;
-  background-color:transparent;
+  background-color: transparent;
 }
 
 .inputGroup :is(input:focus, input:valid) {
   border-color: black;
 }
-.btn-danger{
-    margin-right: 27px;
-    width: 135px;
+.btn-danger {
+  margin-right: 27px;
+  width: 135px;
 }
-.btn-primary{
-    width: 135px;
-    
+.btn-primary {
+  width: 135px;
 }
 </style>
